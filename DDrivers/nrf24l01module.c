7a3d3a660d4f454b550558825ef3177e7bbe6252
João Bento <jbentox98@gmail.com>
2022-01-19 22:00:28 +0000

    ddriver test 22h

--- DDrivers/nrf24l01module.c
+++ DDrivers/nrf24l01module.c
@@ -13,12 +13,33 @@
 //#include <mach/platform.h>
 
 #include "utils.h"
-
+#include "nrf24l01.h"
 
 #define DEVICE_NAME "nrf24"
 #define CLASS_NAME "nrf24Class"
 #define NRF_CE 17
 
+
+#define R 0
+#define W 1
+
+
+#define DATA_PIPE_0 (1<<0)
+#define DATA_PIPE_1 (1<<1)
+#define DATA_PIPE_2 (1<<2)
+#define DATA_PIPE_3 (1<<3)
+#define DATA_PIPE_4 (1<<4)
+#define DATA_PIPE_5 (1<<5)
+#define DATA_PIPE_ALL 0xFF
+
+#define SPEED_1Mbps 0
+#define SPEED_2Mbps 1
+#define SPEED_250Kbps 4
+
+#define POWER_18DB 0
+#define POWER_12DB 1
+#define POWER_6DB 2
+#define POWER_0DB 3
 // static const int spiCSPin = 8;
 // static const int spiMISOPin = 9;
 // static const int spiMOSIPin = 10;
@@ -37,6 +58,28 @@ static struct device *s_pDeviceObject;
 
 static struct spi_device *my_spi_device;
 
+
+uint8_t *nrf_xfer(uint8_t reg, size_t count, uint8_t *val, bool op);
+void nrf_enable_AA(uint8_t pipe);
+void nrf_enable_pipes(uint8_t pipe);
+int nrf_set_retrans_delay(uint8_t delay);
+int nrf_set_addr_width(uint8_t width);
+int nrf_set_retrans_count(uint8_t count);
+int nrf_set_channel(uint8_t channel);
+int nrf_set_speed(uint8_t speed);
+int nrf_set_power(uint8_t power);
+int nrf_set_rx_addr(uint8_t *val, uint8_t pipe);
+void nrf_set_tx_addr(uint8_t *val);
+int nrf_set_rx_width(uint8_t width, uint8_t pipe);
+int is_tx_full(void);
+int is_tx_empty(void);
+int is_rx_full(void);
+int is_rx_empty(void);
+void nrf_enable_dynpd(uint8_t pipe);
+void nrf_power_up(void);
+void nrf_power_down(void);
+void nrf_set_as_recv(void);
+void nrf_set_as_trans(void);
 
 void spi_write( uint8_t data ) //for configuration purposes
 {
@@ -102,6 +145,9 @@ static int reg_spi_device(void)
 
 static int reg_dev(void)
 {
+	int ret;
+
+	struct device *dev_ret;
 
 	if ((ret = alloc_chrdev_region(&nrf24Device_majorminor, 0, 1, DEVICE_NAME)) < 0) {
 		return ret;
