43eba927783b209d639159c2c87e84e1f9ad23b0
Luís Conceição <luiscarlostc92@gmail.com>
2022-02-04 14:42:07 +0000

    select training

--- src/guinewtraining.cpp
+++ src/guinewtraining.cpp
@@ -42,17 +42,20 @@ void GuiNewTraining::on_push_Select_clicked()
     int32_t ret;
     QString train_name;
 
-    //get the training name on the combo seltector
- //   train_name = ui->comboBox_ChooseTrain->itemData().toString;
-    //set the "to play" trianing based on the name from the combo box
-//    ret = this->guiUser->setToPlayTraining(QString::toStdString(train_name));
-//    if(ret == 0)
-//    {
-        this->hide();
-        guiTrainingSession->show();
-//    }else {
-//        QMessageBox::warning(this, "Add New Model", "Could not set training");
-//    }
+    train_name = ui->comboBox_ChooseTrain->itemData(ui->comboBox_ChooseTrain->currentIndex()).toString();
+    train_name = ui->comboBox_ChooseTrain->currentText();
+
+    if(train_name == "")
+    {
+        QMessageBox::warning(this, "Select Model", "Please choose a Model");
+        return;
+    }
+
+    this->guiUser->setToPlayTraining(train_name.toStdString());
+
+    this->hide();
+    guiTrainingSession->show();
+
 }
 
 
